e4100c95fda2c9972cf8e9dfdbec54739776a291
AwthanSoft <softmawa@gmail.com>
2021-08-22 03:06:22 +0300

    Info Changed

--- AutoCompleteTextBoxMe.WPF/Properties/AssemblyInfo.cs
+++ AutoCompleteTextBoxMe.WPF/Properties/AssemblyInfo.cs
@@ -10,10 +10,10 @@ using System.Windows;
 [assembly: AssemblyTitle("AutoCompleteTextBoxMe.WPF")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("AwthanSoft")]
 [assembly: AssemblyProduct("AutoCompleteTextBoxMe.WPF")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Awthan")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("21.08.22.0")]
+[assembly: AssemblyFileVersion("21.08.22.0")]
